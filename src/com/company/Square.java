d7a42d0497d89d79428a6ccebe19fff4d424dd44
richaar <arianna.richardson@ssfs.org>
2020-01-13 14:11:38 -0500

    Not finished

---
+++ src/com/company/Square.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Square {
+
+private double side;
+
+public Square square = new Square(double s);
+
+Square(s)
+
+{
+    s = side;
+}
+
+public double calculateArea() {
+   double area = side * side;
+}
+
+    public String toString()
+    {
+        return "Square with side " + side ;
+    }
+
+}
+
