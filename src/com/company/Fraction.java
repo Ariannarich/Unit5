0066d952c3dd3b902b4095653fb3245b9eb325c6
arian <arianna.richardson@ssfs.org>
2020-02-07 08:12:25 -0500

    Started Fraction

--- src/com/company/Fraction.java
+++ src/com/company/Fraction.java
@@ -7,16 +7,24 @@ public class Fraction {
     public Fraction(int numerator, int denominator) {
         this.numerator = numerator;
         this.denominator = denominator;
+         }
 
-        if (denominator < 0) {
-            denominator = Math.abs(denominator);
-            numerator -= numerator * 2;
-        }
-    }
         public Fraction()
         { this(1,1);
     }
-public void reduce() {
+/*
+    private int reduce ( int m, int n){
+        while (m % n != 0) {
+            int oldm = m;
+            int oldn = n;
+
+            m = oldn;
+            n = oldm % oldn;
+        }
+        return n;
+    }
+*/
+public static void reduce(Fraction f1, Fraction f2) {
     for (int i = 0; i < denominator; i++) {
         if (numerator % i == 0 && denominator % i == 0) {
             numerator %= i;
@@ -24,28 +32,85 @@ public void reduce() {
         }
     }
 }
-    public static void addPositive(Fraction f1, Fraction f2)
-            {
-             int  newDen =  f1.denominator * f2.denominator;
-                int num1 = f1.numerator * f2.denominator;
-                int num2 = f2.numerator * f1.denominator;
-                int newNum = num1 + num2;
-                return new Fraction() Fraction(newNum, newDen);
-            }
 
 
-          /*  if (numerator % 2 == 0 && denominator % 2 == 0) {
-                numerator %= 2;
-                denominator %= 0;
-            }
-        if (numerator % 2 == 0 && denominator % 2 == 0) {
-            numerator %= 2;
-            denominator %= 0;
-        }
-        if (numerator % 2 == 0 && denominator % 2 == 0) {
-            numerator %= 2;
-            denominator %= 0;
-        }
 
-           */
+
+    public static Fraction add(Fraction f1, Fraction f2)
+    {
+
+    simplify(f1,f2);
+        int newDen = f1.denominator * f2.denominator;
+        int num1 = f1.numerator * f2.denominator;
+        int num2 = f2.numerator * f1.denominator;
+        int newNum = num1 + num2;
+        reduce(f1,f2);
+        return new Fraction(newNum, newDen);
+    }
+
+    public static Fraction subtract(Fraction f1, Fraction f2)
+    {
+        simplify(f1,f2);
+        int newDen = f1.denominator * f2.denominator;
+        int num1 = f1.numerator * f2.denominator;
+        int num2 = f2.numerator * f1.denominator;
+        int newNum = num1 - num2;
+       return new Fraction(newNum, newDen);
+    }
+    public static Fraction multiply(Fraction f1, Fraction f2)
+    {
+        simplify(f1,f2);
+        int newDen = f1.denominator * f2.denominator;
+        int newNum = f1.numerator * f2.numerator;
+        return new Fraction(newNum, newDen);
+    }
+    public static Fraction divide(Fraction f1, Fraction f2)
+    {
+        simplify(f1,f2);
+        int newDen = f1.denominator * f2.numerator;
+        int newNum = f1.numerator * f2.denominator;
+        return new Fraction(newNum, newDen);
+    }
+    public static Fraction addNeg(Fraction f1, Fraction f2)
+    {
+        simplify(f1,f2);
+        int newDen = f1.denominator * f2.denominator;
+        int num1 = f1.numerator * f2.denominator;
+        int num2 = f2.numerator * f1.denominator;
+        int newNum = num1 + num2;
+        return new Fraction(newNum, newDen);
+    }
+
+    public static Fraction subtractNeg(Fraction f1, Fraction f2)
+    {
+        simplify(f1,f2);
+        int newDen = f1.denominator * f2.denominator;
+        int num1 = f1.numerator * f2.denominator;
+        int num2 = f2.numerator * f1.denominator;
+        int newNum = num1 - num2;
+        return new Fraction(newNum, newDen);
+    }
+    public static Fraction multiplyNeg(Fraction f1, Fraction f2)
+    {
+        simplify(f1,f2);
+        int newDen = f1.denominator * f2.denominator;
+        int newNum = f1.numerator * f2.numerator;
+        return new Fraction(newNum, newDen);
+    }
+    public static Fraction divideNeg(Fraction f1, Fraction f2)
+    { simplify(f1,f2);
+        int newDen = f1.denominator * f2.numerator;
+        int newNum = f1.numerator * f2.denominator;
+        return new Fraction(newNum, newDen);
+    }
+    public static void simplify(Fraction f1, Fraction f2) {
+        if (f1.denominator < 0) {
+            f1.denominator = Math.abs(f1.denominator);
+            f1.numerator -= f1.numerator * 2;
+        } else if (f2.denominator < 0) {
+            f2.denominator = Math.abs(f2.denominator);
+            f2.numerator -= f2.numerator * 2;
         }
+    }
+
+}
