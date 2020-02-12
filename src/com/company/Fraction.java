44e149e6003056586b5972f853a4a359cc5e773a
arian <arianna.richardson@ssfs.org>
2020-02-11 21:59:24 -0500

    Finished Fraction

--- src/com/company/Fraction.java
+++ src/com/company/Fraction.java
@@ -1,30 +1,54 @@
 package com.company;
 
+/**
+ * @author Arianna Richardson
+ * @since February 5th, 2020.
+ * This program calculates the values of two different fractions using arithmetic equations.
+ */
+
+/**
+ * Start code:
+ * Declares the numerator and denominator variables
+ */
 public class Fraction {
     private int numerator;
     private int denominator;
 
-    public Fraction(int numerator, int denominator) {
-        if (this.denominator < 0 && this.numerator > 0)
-        { this.denominator = Math.abs(this.denominator);
-            this.numerator = 0- this.numerator;
-        }
-        else if(this.denominator < 0 && this.numerator > 0)
-        {this.denominator = Math.abs(this.denominator);
-            this.numerator = 0-this.numerator;
-        }
+    /**
+     * Constructor for Fraction object.
+     * @param numerator
+     * @param denominator
+     * Also, simplifies fractions using the greatest common divisor,
+     * and switches negative sign from the denominator to the numerator.
+     */
+    public Fraction(int numerator, int denominator)
+    {
         this.numerator = numerator;
         this.denominator = denominator;
-        int gcd = reduce(this.numerator, this.denominator);
+
+        int gcd = Math.abs(reduce(this.numerator, this.denominator));
+        if(this.denominator < 0 )
+        {this.denominator = Math.abs(this.denominator);
+            this.numerator = -this.numerator;
+        }
         this.numerator/=gcd;
         this.denominator/=gcd;
          }
 
+    /**
+     * Constructor for a default Fraction object.
+     */
         public Fraction()
         { this(1,1);
     }
 
-    private int reduce ( int m, int n){
+    /**
+     * Finds the greatest common divisor to reduce fractions with.
+     * @param m
+     * @param n
+     * @return
+     */
+   private int reduce ( int m, int n){
         while (m % n != 0) {
             int oldm = m;
             int oldn = n;
@@ -36,26 +60,15 @@ public class Fraction {
     }
 
 
-
-
-
-/*
-public static void reduce(Fraction f1, Fraction f2) {
-    for (int i = 0; i < denominator; i++) {
-        if (numerator % i == 0 && denominator % i == 0) {
-            numerator %= i;
-            denominator %= i;
-        }
-    }
-}
-*/
-
-
-
+    /** method adds two fractions together and returns new Fraction object.
+     * @param f1 Fraction object 1
+     * @param f2 Fraction object 2
+     *
+     * @return returns sum as a new Fraction object.
+     */
     public static Fraction add(Fraction f1, Fraction f2)
     {
 
-    simplify(f1,f2);
         int newDen = f1.denominator * f2.denominator;
         int num1 = f1.numerator * f2.denominator;
         int num2 = f2.numerator * f1.denominator;
@@ -63,37 +76,54 @@ public static void reduce(Fraction f1, Fraction f2) {
         return new Fraction(newNum, newDen);
     }
 
+    /**
+     * method subtracts two fractions from each other
+     * @param f1 Fraction object 1
+     * @param f2 Fraction object 2
+     *
+     * @return returns difference as a new Fraction object.
+     */
     public static Fraction subtract(Fraction f1, Fraction f2)
     {
-        simplify(f1,f2);
         int newDen = f1.denominator * f2.denominator;
         int num1 = f1.numerator * f2.denominator;
         int num2 = f2.numerator * f1.denominator;
         int newNum = num1 - num2;
        return new Fraction(newNum, newDen);
     }
+    /**
+     * method multiplies two fractions together and returns new Fraction object.
+     * @param f1 Fraction object 1
+     * @param f2 Fraction object 2
+     *
+     * @return returns product as a new Fraction object.
+     */
     public static Fraction multiply(Fraction f1, Fraction f2)
     {
-        simplify(f1,f2);
         int newDen = f1.denominator * f2.denominator;
         int newNum = f1.numerator * f2.numerator;
         return new Fraction(newNum, newDen);
     }
+    /**
+     * method divides two fractions and returns new Fraction object.
+     * @param f1 Fraction object 1
+     * @param f2 Fraction object 2
+     *
+     * @return returns quotient as a new Fraction object.
+     */
     public static Fraction divide(Fraction f1, Fraction f2)
     {
-        simplify(f1,f2);
         int newDen = f1.denominator * f2.numerator;
         int newNum = f1.numerator * f2.denominator;
         return new Fraction(newNum, newDen);
     }
 
-    public static void simplify(Fraction f1, Fraction f2) {
 
-
-
-        }
-
-public String toString(){
+    /**
+     * String method that prints fraction answer from equations
+     * @return returns fraction as string
+     */
+    public String toString(){
         return numerator+"/"+denominator;
 }
 }
